96545bf5d7088832f8d08d6d633059aee548e061
Mae Capozzi <maecapozzi@gmail.com>
2017-09-07 07:52:56 -0400

    Add ability to display data for each song

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Results = props => (
   <div>
-    {console.log(props.danceability)}
+    <p>danceability: {props.danceability}</p>
+    <p>acousticness: {props.acousticness}</p>
+    <p>energy: {props.energy}</p>
+    <p>liveness: {props.liveness}</p>
+    <p>instrumentalness: {props.instrumentalness}</p>
+    <p>loudness: {props.loudness}</p>
+    <p>speechiness: {props.speechiness}</p>
+    <p>valence: {props.valence}</p>
   </div>
 )
 
--- src/components/SongLink.js
+++ src/components/SongLink.js
@@ -5,21 +5,25 @@ import Results from './Results'
 class SongLink extends Component {
   constructor (props) {
     super(props)
-    this.state = {
-      id: this.props.id
-    }
+    this.state = {}
 
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick (event) {
     event.preventDefault()
-    axios.get('/analyze/' + this.state.id)
+    axios.get('/analyze/' + this.props.id)
     .then((response) => {
       if (response.status === 200) {
         this.setState({
-          danceability: response.data.danceability[0],
-          acousticness: response.data.acousticness[0]
+          danceability: response.data.danceability,
+          acousticness: response.data.acousticness,
+          energy: response.data.energy,
+          liveness: response.data.liveness,
+          instrumentalness: response.data.instrumentalness,
+          loudness: response.data.loudness,
+          speechiness: response.data.speechiness,
+          valence: response.data.valence
         })
       }
     })
@@ -40,6 +44,9 @@ class SongLink extends Component {
     }
     return (
       <div>
+        <li onClick={this.handleClick}>
+          {this.props.name} by {this.props.artist}
+        </li>
         <Results danceability={this.state.danceability} acousticness={this.state.acousticness} />
       </div>
     )
