3fd8c43a330d50f1c3b40fe30122c96c1659742c
Mae Capozzi <maecapozzi@gmail.com>
2017-09-13 18:15:05 -0400

    Add authentication flow

--- src/components/SpotifyAuthButton.js
+++ src/components/SpotifyAuthButton.js
@@ -1,36 +1,7 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React from 'react'
 
-class SpotifyAuthButton extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick (event) {
-    event.preventDefault()
-    axios.get('/login')
-      .then((response) => {
-        if (response.status === 200) {
-          window.location = response.data
-        }
-      })
-      .catch((error) => {
-        console.log(error)
-      }
-    )
-  }
-
-  render () {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Login</button>
-
-      </div>
-    )
-  }
-}
+const SpotifyAuthButton = () => (
+  <button><a href='http://localhost:3001/auth/spotify'>Login with Spotify</a></button>
+)
 
 export default SpotifyAuthButton
