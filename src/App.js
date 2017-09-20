115124dea2f7f619228556017934fdd0fa8321ea
Mae Capozzi <maecapozzi@gmail.com>
2017-09-20 18:09:24 -0400

    add full url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
 
   componentWillMount () {
-    axios.get('/tokens')
+    axios.get('https://spotify-viz-api/tokens')
     .then((response) => {
       if (response.data === 'OK') {
         this.setState({ authenticateInProgress: true })
