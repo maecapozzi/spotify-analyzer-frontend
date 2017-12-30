fe9173df8b913f67145d6b3eb96ae2505fa85928
maecapozzi <maecapozzi@gmail.com>
2017-12-30 14:22:18 -0500

    Hotfix for hitting wrong endpoint

--- src/api-config.js
+++ src/api-config.js
@@ -1,7 +1,7 @@
 let backendHost
 const hostname = window && window.location && window.location.hostname
 
-if (hostname === 'ear-worm.com' || hostname === 'spotify-viz-frontend.herokuapp.com') {
+if (hostname === 'https://www.ear-worm.com' || hostname === 'https://www.spotify-viz-frontend.herokuapp.com') {
   backendHost = 'https://spotify-viz-api.herokuapp.com'
 } else {
   backendHost = 'http://localhost:3001'
